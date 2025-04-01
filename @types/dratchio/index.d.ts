31102df85a3ff749a20acea4d832f33afdc18367
Tjoseph-O <mrtemitopejoseph@gmail.com>
2025-04-01 05:11:27 +0100

    update

--- @types/dratchio/index.d.ts
+++ @types/dratchio/index.d.ts
@@ -1,13 +1,30 @@
-// BackEnd/@types/dratchio/index.d.ts
-declare module 'dratchio' {
-    export class DratchioClient {
-      constructor(config: any);
-      on(event: string, callback: Function): DratchioClient;
-      register(): Promise<void>;
-      call(options: { to: string, from: string }): Promise<{
-        uuid: string;
-        answer: () => void;
-        hangup: () => void;
-      }>;
+// // BackEnd/@types/dratchio/index.d.ts
+// import { User } from '../../src/modules/users/entities/user.entity'
+//
+// declare module 'dratchio' {
+//     export class DratchioClient {
+//       constructor(config: any);
+//       on(event: string, callback: Function): DratchioClient;
+//       register(): Promise<void>;
+//       call(options: { to: string, from: string }): Promise<{
+//         uuid: string;
+//         answer: () => void;
+//         hangup: () => void;
+//       }>;
+//     }
+//   }
+
+// @types/express/index.d.ts
+import { User } from '../../src/modules/users/entities/user.entity';
+
+declare global {
+  namespace Express {
+    // Extend the Request interface
+    interface Request {
+      user?: User;
     }
   }
+}
+
+// This empty export is required to make this a module
+export {};
