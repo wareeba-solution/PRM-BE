78f992fcb67cf25ed6f20ee247afab29781e0511
Tjoseph-O <mrtemitopejoseph@gmail.com>
2025-04-07 15:50:56 +0100

    fit: error

---
+++ src/config/swagger-dto.ts
@@ -0,0 +1,6 @@
+/**
+ * Re-export file to fix module resolution in production
+ * This file ensures that the import from './swagger-dto' in swagger.config.ts works correctly
+ */
+
+export * from './swagger-dto/index';
